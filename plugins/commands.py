6c316b182ee658e2c4f479c95f70a41107a0749d
Dilpreet3 <tejasingh7786@gmail.com>
2024-07-28 13:45:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -54,10 +54,10 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ᴍʏ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ', url='https://youtube.com/@Tech_VJ')
+            InlineKeyboardButton('🔞 Viral Video 🔞', url=https://t.me/officail_cute_balika')
             ],[
-            InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/vj_bot_disscussion'),
-            InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/vj_botz')
+            InlineKeyboardButton('Hentai Hindi', url='https://t.me/+NsiuGo4c-qg2N2M1'),
+            InlineKeyboardButton('Desi Viral Video 🥵', url='https://t.me/+4tf113saVmliYmI1')
             ],[
             InlineKeyboardButton('💁‍♀️ ʜᴇʟᴘ', callback_data='help'),
             InlineKeyboardButton('😊 ᴀʙᴏᴜᴛ', callback_data='about')
